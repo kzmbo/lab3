23d5c2bedd21e1d7dd775712c4e891f16b7209ec
Alex <alexkem132@gmail.com>
2021-09-16 18:50:14 -0700

    first commit

---
+++ src/Cart.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Cart {
+	
+	private ArrayList<Product> purchasedProducts;
+	private double total;
+	
+	
+	public Cart() {
+		//this.purchasedProducts = //FIXME: Complete this line;
+		//this.total = 0;//FIXME: complete this line;
+	}
+	
+	public void addProduct(Product p) {
+		// FIXME: complete the implementation of this method.
+	}
+	
+	
+	public double getTransactionTotal() {
+		// FIXME: complete the implementation of this method.
+	}
+	
+	/**
+	* returns a formatted string containing the entire 
+	* representation of the receipt for this cart,
+	* including store name, list of items with subtotals,
+	* grand total, payment, and change.
+	* @return a String 
+	*/
+	public String toString() {
+		return "FIXME: return the correct string";
+	}
+
+}
---
+++ src/Product.java
@@ -0,0 +1,66 @@
+public class Product {
+	
+	private String productSKU;
+	private String productName;
+	private String productSize;
+	private double productPrice;
+	
+	/**
+	 * creates a Product object with given name, size, and price
+	 * @param productName - the name of this product 
+	 * @param productSize - the size of this product as a string
+	 * @param productPrice  - the price of this product as a double
+	 * **/
+	public Product(String productSKU, String productName, String productSize, double productPrice) {
+		this.productSKU = productSKU;
+		this.productName = productName;
+		this.productSize = productSize;
+		this.productPrice = productPrice;
+	}
+	
+	
+	public String getName() {
+		return "TODO: Implement and document getName()";
+	}
+	
+	public String getSKU() {
+		return "TODO: Implement and document getSKU()";
+	}
+	
+	public String getSize() {
+		return "TODO: Implement and document getSize()";
+	}
+	
+	public double getPrice() {
+		System.out.println("TODO: Implement and document getName()");
+		return 0;
+	}
+	
+
+	
+	/**
+	 * overrides equals() so that two Product objects are considered equal if the
+	 * strings representing their SKU numbers are equal
+	 * */
+	public boolean equals(Object other) {
+		
+		Product otherProduct = (Product) other;
+		
+		if(this.productSKU.equals(otherProduct.getSKU())) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("--------------PRODUCT TESTER-----------------");
+		Product p = new Product("12354","Cookies", "12 dz", 3.50);
+		System.out.println("Printing object: "+p);
+		System.out.println("Expected: Cookies                       12 dz     $3.50");
+	}
+
+}
